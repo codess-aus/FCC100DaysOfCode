8639713be90f92bc9fe2649bdc9814c7cba359b3
Michelle Mei-Ling Sandford <msandford@outlook.com>
2020-09-16 21:48:31 +0800

    pigs

---
+++ pig2.js
@@ -0,0 +1,57 @@
+/*
+Intermediate Algorithm Scripting: Pig Latin
+Pig Latin is a way of altering English Words. The rules are as follows:
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+
+- If a word begins with a vowel, just add "way" at the end.
+
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+
+Problem Explanation
+You need to create a program that will translate from English to Pig Latin. Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an “ay”. If a word begins with a vowel you just add “way” to the end. It might not be obvious but you need to remove all the consonants up to the first vowel in case the word does not start with a vowel.
+
+Hint 1
+You will probably want to use regular expressions. This will allow you to convert the words easily.
+
+Hint 2
+If the first character is a vowel, then take that whole word and add ‘way’ at the end. Otherwise comes the tricky part, take the consonant(s) before the first vowel and move it to the end and add ‘ay’. This might be confusing but, it is not just the first consonant but all of them before the first vowel.
+
+Hint 3
+You will need to use everything you know about string manipulation to get the last part right. However, it can be done with substr alone.
+
+*/
+
+function translatePigLatin(str) {
+    // Create variables to be used
+    var pigLatin = "";
+    var regex = /[aeiou]/gi;
+  
+    // Check if the first character is a vowel
+    if (str[0].match(regex)) {
+      pigLatin = str + "way";
+    } else if (str.match(regex) === null) {
+      // Check if the string contains only consonants
+      pigLatin = str + "ay";
+    } else {
+      // Find how many consonants before the first vowel.
+      var vowelIndice = str.indexOf(str.match(regex)[0]);
+  
+      // Take the string from the first vowel to the last char
+      // then add the consonants that were previously omitted and add the ending.
+      pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+    }
+
+  /*
+Code Explanation
+Make an empty string to hold your Pig Latin word.
+Assign your appropriate regular expression to a variable.
+If the first character is a vowel, just add way to end of string and return it.
+If the first character is not a vowel:
+Find number of consonants before first vowel with help of indexOf(), match() and regex.
+Start Pig Latin string with first vowel till the end.
+Add letters before first vowel to end of string.
+substr() is used for string manipulation here.
+Add ay to end of string and return it.
+
+  */
